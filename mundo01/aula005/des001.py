fb7072d3d15c29feb319d2e970f0031f97ec057e
jao11 <46947076+jao11@users.noreply.github.com>
2019-03-20 11:25:08 -0300

    Add files via upload

---
+++ mundo01/aula005/des001.py
@@ -0,0 +1,3 @@
+nome = input('Qual o seu nome?-')
+print('Seja bem vindo', nome,'.')
+print('É um prazer te conhecer {}!'.format(nome))
---
+++ mundo01/aula005/des002.py
@@ -0,0 +1,5 @@
+print('Você poderia digitar a sua data de aniversário??')
+dia = input('Dia:')
+mes = input('Mês:')
+ano = input('Ano:')
+print('Você nasceu mesmo no dia', dia, 'de', mes, 'de', ano, '? Nossa que legal eu também!!')
